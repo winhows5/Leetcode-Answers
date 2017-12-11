24e63f239db7dbcd627885304c4bf0ca2b7fe617
winhows5 <wangwh258@gmail.com>
2017-12-11 22:36:32 +0800

    Create ans1.cpp

---
+++ 9. Palindrome Number/ans1.cpp
@@ -0,0 +1,22 @@
+/* time cost: 42ms (99.87%) */
+/* time complexity: O(n)    */
+static int x = [](){
+    std::ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    return 0;
+}();
+
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        if (x<0) return false;
+        string s = to_string(x);
+        char c;
+        for (int i=0, j=s.size()-1; i < j; ++i, --j) {
+            if (s[i] != s[j])
+                return false;
+        }
+        return true;
+        
+    }
+};
