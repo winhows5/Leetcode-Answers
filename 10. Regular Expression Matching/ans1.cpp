fa98d76558d683d485081c5c017a3a34d6139e07
winhows5 <wangwh258@gmail.com>
2017-12-18 16:59:59 +0800

    Update ans1.cpp

--- 10. Regular Expression Matching/ans1.cpp
+++ 10. Regular Expression Matching/ans1.cpp
@@ -1,1 +1,36 @@
+/* dynamic programming     */
+/* time cost: 3ms (99.96%) */
+/* time complexity: O(mn)  */
 
+static int x = [](){
+    std::ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    return 0;
+}();
+class Solution {
+public:
+    bool isMatch(string s, string p) {
+    int m = s.size(), n = p.size();
+    bool d[m+1][n+1];
+    for(int i = m; i >= 0; --i)
+        for (int j = n; j >=0; --j) {
+            d[i][j] = false;
+        }
+
+    bool front;
+    for(int i = m; i >= 0; --i)
+        for (int j = n; j >= 0; --j) {
+            front = i < m && j < n && (s[i] == p[j] || p[j] == '.');
+            if (i == m && j == n) {d[i][j] = true; continue;}
+            if (p[j+1] == '*'){
+                if(j + 2 <= n)
+                    d[i][j] = d[i][j+2] || (front && d[i+1][j]);
+                else d[i][j] = front && d[i+1][j];
+            }
+            else d[i][j] = front && d[i+1][j+1];
+        }
+
+    return d[0][0];
+    
+}
+};
