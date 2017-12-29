920736d5ed47b498c5d7531229b2167b862f8e47
winhows5 <wangwh258@gmail.com>
2017-12-29 12:18:17 +0800

    Create ans1.cpp

---
+++ 0022. Generate Parentheses/ans1.cpp
@@ -0,0 +1,39 @@
+/* time cost: 3ms (9.67%) */
+/* time complexity: O(n)  */ 
+static int x = [](){
+    std::ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    return 0;
+}();
+
+class Solution {
+public:
+    vector<string> generateParenthesis(int n) {
+        if(!n) return {};
+        vector<string> Ans{"("};
+        vector<vector<int>> k{1, {n - 1, 1}};
+
+        while(1) {
+            for (int i = 0; i < Ans.size(); ++i) {
+                int r = k[i][1];
+                if (k.size() > i && k[i][0]) {
+                    if (r) {
+                        k.push_back({k[i][0] - 1, r + 1});
+                        Ans.push_back(Ans[i] + '(');
+                    }
+                    else {
+                        --k[i][0];
+                        ++r;
+                        Ans[i] += '(';
+                    }
+                }
+                if (k.size() > i && k[i][1]) {
+                    Ans[i] += ')';
+                    --r;
+                }
+                k[i][1] = r;
+            }
+            if (k[0][1] == 0 && k[0][0] == 0) return Ans;
+        }
+    }
+};
