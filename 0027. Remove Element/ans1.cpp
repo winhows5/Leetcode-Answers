c0cce24704e7d5ba905f140a77aa86066e1ad9c4
winhows5 <wangwh258@gmail.com>
2018-01-08 00:06:05 +0800

    Update ans1.cpp

--- 0027. Remove Element/ans1.cpp
+++ 0027. Remove Element/ans1.cpp
@@ -1,1 +1,23 @@
+/* time cost: 5ms (61.14%) */
+/* time complexity: O(n)   */
 
+static int x = [](){
+    std::ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    return 0;
+}();
+
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        if(nums.size() == 0) return 0;
+        int i = -1;
+        for (int j = 0; j < nums.size(); ++j) {
+            if (nums[j] != val) {
+                ++i;
+                nums[i] = nums[j];
+            }
+        }
+        return i+1;
+    }
+};
