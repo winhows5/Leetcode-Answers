f95f0c4c1de5197fe55a127b273dd63ee113b79f
winhows5 <wangwh258@gmail.com>
2018-02-23 12:20:48 +0800

    Update ans1.cpp

--- 0046. Permutations/ans1.cpp
+++ 0046. Permutations/ans1.cpp
@@ -1,2 +1,30 @@
-/* time cost: */
-/* time complexity: O() */
+/* time cost: 12ms (53.87%) */
+/* time complexity: O(n)    */
+
+static int x = [](){
+    std::ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    return 0;
+}();
+
+class Solution {
+public:
+    vector<vector<int>> permute(vector<int>& nums) {
+        vector<vector<int>> ans;
+        permuteRecursion(nums, 0, ans);
+        return ans;
+    }
+    
+    void permuteRecursion(vector<int> &nums, int ind, vector<vector<int>> &ans) {
+        if (ind == nums.size()-1) {
+            ans.push_back(nums);
+            return;
+        }
+        
+        for (int i = ind; i < nums.size(); ++i) {
+            swap(nums[ind],nums[i]);
+            permuteRecursion(nums, ind + 1, ans);
+            swap(nums[ind],nums[i]);
+        }
+    }
+};
