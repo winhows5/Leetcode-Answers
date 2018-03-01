dac912fd84816598b7e75968a8ad2ca3fa3f2582
winhows5 <wangwh258@gmail.com>
2018-03-01 09:40:39 +0800

    Create ans1.cpp

---
+++ 0041. First Missing Positive/ans1.cpp
@@ -0,0 +1,25 @@
+/* time cost: 4ms (51.82%) */
+/* time complexity: O(n)   */
+
+static int x = [](){
+    std::ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    return 0;
+}();
+
+class Solution {
+public:
+    int firstMissingPositive(vector<int>& nums) {
+        int n = nums.size();
+        for (int i = 0; i < n; ++i) {
+            while (nums[i] > 0 && nums[i] <= n && nums[i] != nums[nums[i]-1]) {
+                swap(nums[i], nums[nums[i] - 1]);
+            }
+        }
+        for (int i = 0; i < n; ++i) {
+            if (nums[i] != i+1)
+                return i+1;
+        }
+        return n+1;
+    }
+};
