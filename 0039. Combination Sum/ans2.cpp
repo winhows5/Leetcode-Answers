2fac4b54b30a91eb205b795a515d64c75fb4d3a5
winhows5 <wangwh258@gmail.com>
2018-03-02 17:13:50 +0800

    Create ans2.cpp

---
+++ 0039. Combination Sum/ans2.cpp
@@ -0,0 +1,34 @@
+/* time cost: 15ms (41.65%) */
+/* time complexity: O(n^k)  */
+
+static int x = [](){
+    std::ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    return 0;
+}();
+
+class Solution {
+public:
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        vector<vector<int>> ans;
+        vector<int> t_ans;
+        sort(candidates.begin(), candidates.end());
+        iteration(candidates, 0, target, t_ans, ans);
+        return ans;
+    }
+    
+    void iteration(vector<int> candidates, int ind, int target, vector<int> t_ans, vector<vector<int>> &ans) {
+        if (ind == candidates.size()) return;
+        int t = candidates[ind];
+        while(target >= 0) {
+            if (target == 0) {
+                ans.push_back(t_ans);
+                return;
+            }
+            if (target < t) return;
+            iteration(candidates, ind + 1, target, t_ans, ans);
+            target -= t;
+            t_ans.push_back(t);
+        }
+    }
+};
