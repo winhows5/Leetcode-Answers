f48fd3092858f51122a7731c2249b9d3b764183c
winhows5 <wangwh258@gmail.com>
2018-03-15 20:22:30 +0800

    Update ans1.cpp

--- 0037. Sudoku Solver/ans1.cpp
+++ 0037. Sudoku Solver/ans1.cpp
@@ -1,4 +1,4 @@
-/* time cost: 2ms (98.35%) */
+/* time cost: 2ms (100.00%) */
 /* time complexity: O(-)   */
 
 // cannot solve this one in 20min ABSOLUTELY:(
@@ -9,9 +9,9 @@ static int x = [](){
 }();
 
 class Solution {
-	struct cell
+	struct cell // encapsulates a single cell on a Sudoku board
 	{
-		int value; 
+		int value; // cell value 1..9 or 0 if unset
 		// number of possible (unconstrained) values for the cell
 		int numPossibilities;
 		// if bitset[v] is 1 then value can't be v
@@ -24,17 +24,19 @@ class Solution {
 	// the function also propagates constraints to other cells and deduce new values where possible
 	bool set(int i, int j, int v)
 	{ 
+		// updating state of the cell
 		cell& c = cells[i][j];
 		if (c.value == v)
 			return true;
 		if (c.constraints[v])
 			return false;
-		c.constraints = bitset<10>(0x3FE); 
+		c.constraints = bitset<10>(0x3FF); // all 1s
 		c.constraints.reset(v);
 		c.numPossibilities = 1;
 		c.value = v;
 
-		for (int k = 0; k<9; k++) {
+		// propagating constraints
+		for (int k = 0; k<9; ++k) {
 			// to the row: 
 			if (i != k && !updateConstraints(k, j, v))
 				return false;
@@ -63,23 +65,23 @@ class Solution {
 		c.constraints.set(excludedValue);
 		if (--c.numPossibilities > 1)
 			return true;
-		for (int v = 1; v <= 9; v++) {
+		for (int v = 1; v <= 9; ++v) {
 			if (!c.constraints[v]) {
 				return set(i, j, v);
 			}
 		}
-		assert(false);
 	}
 
 	// backtracking state - list of empty cells
 	vector<pair<int, int>> bt;
 
+	// find values for empty cells
 	bool findValuesForEmptyCells()
 	{
 		// collecting all empty cells
 		bt.clear();
-		for (int i = 0; i < 9; i++) {
-			for (int j = 0; j < 9; j++) {
+		for (int i = 0; i < 9; ++i) {
+			for (int j = 0; j < 9; ++j) {
 				if (!cells[i][j].value)
 					bt.push_back(make_pair(i, j));
 			}
@@ -104,13 +106,13 @@ class Solution {
 		// slow path >1 possibility.
 		// making snapshot of the state
 		array<array<cell,9>,9> snapshot(cells);
-		for (int v = 1; v <= 9; v++) {
+		for (int v = 1; v <= 9; ++v) {
 			if (!constraints[v]) {
 				if (set(i, j, v)) {
 					if (backtrack(k + 1))
 						return true;
 				}
-
+				// restoring from snaps hot,
 				// note: computationally this is cheaper
 				// than alternative implementation with undoing the changes
 				cells = snapshot;
@@ -123,9 +125,10 @@ public:
 		cells = array<array<cell,9>,9>(); // clear array
 
 		// As we do it - constraints are propagated and even additional values are set as we go
-		for (int i = 0; i < 9; i++)
+		// (in the case if it is possible to unambiguously deduce them).
+		for (int i = 0; i < 9; ++i)
 		{
-			for (int j = 0; j < 9; j++) {
+			for (int j = 0; j < 9; ++j) {
 				if (board[i][j] != '.' && !set(i, j, board[i][j] - '0'))
 					return; // sudoku is either incorrect or unsolvable
 			}
@@ -136,9 +139,9 @@ public:
 			return; // sudoku is unsolvable
 
 		// copying the solution back to the board
-		for (int i = 0; i < 9; i++)
+		for (int i = 0; i < 9; ++i)
 		{
-			for (int j = 0; j < 9; j++) {
+			for (int j = 0; j < 9; ++j) {
 				if (cells[i][j].value)
 					board[i][j] = cells[i][j].value + '0';
 			}
