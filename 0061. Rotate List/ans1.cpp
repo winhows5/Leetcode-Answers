afc5f9cacaa0840e8c8596fc3b3d324c3374e057
winhows5 <wangwh258@gmail.com>
2018-03-19 19:28:05 +0800

    Update ans1.cpp

--- 0061. Rotate List/ans1.cpp
+++ 0061. Rotate List/ans1.cpp
@@ -1,2 +1,40 @@
-/* time cost: 5ms () */
-/* time complexity: O(n) */
+/* time cost: 10ms (100.00%) */
+/* time complexity: O(n)     */
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+static int x = [](){
+    std::ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    return 0;
+}();
+
+class Solution {
+public:
+    ListNode* rotateRight(ListNode* head, int k) {
+        if (!head) return head;
+        ListNode preHead(-1);
+        ListNode mid(-1);
+        preHead.next = head;
+        int lgh = 1;
+        while (head->next) {
+            head = head->next;
+            ++lgh;
+        }
+        int cnt = lgh - k % lgh;
+        if (cnt == lgh) return preHead.next;
+        head->next = preHead.next;
+        while(--cnt) {
+            preHead.next = (preHead.next)->next;
+        }
+        mid.next = (preHead.next)->next;
+        (preHead.next)->next = NULL;
+        return mid.next;
+    }
+};
