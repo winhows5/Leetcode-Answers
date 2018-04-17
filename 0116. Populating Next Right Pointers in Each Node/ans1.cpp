e10b24b97788040e3588bc00ebefe048b2977f9b
winhows5 <wangwh258@gmail.com>
2018-04-17 23:32:56 +0800

    Create ans1.cpp

---
+++ 0116. Populating Next Right Pointers in Each Node/ans1.cpp
@@ -0,0 +1,39 @@
+/* time cost: 19ms (91.12%) */
+/* time complexity: O(n)    */
+
+/**
+ * Definition for binary tree with next pointer.
+ * struct TreeLinkNode {
+ *  int val;
+ *  TreeLinkNode *left, *right, *next;
+ *  TreeLinkNode(int x) : val(x), left(NULL), right(NULL), next(NULL) {}
+ * };
+ */
+static int x = [](){
+    std::ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    return 0;
+}();
+
+class Solution {
+public:
+    void connect(TreeLinkNode *root) {
+        if (!root) return;
+        queue<TreeLinkNode*> nodes;
+        TreeLinkNode *pre, *cur;
+        nodes.push(root);
+        while(!nodes.empty()) {
+            int n = nodes.size();
+            pre = nodes.front();
+            for (int i = 0; i < n; ++i) {
+                cur = nodes.front();
+                nodes.pop();
+                if (cur->left) nodes.push(cur->left);
+                if (cur->right) nodes.push(cur->right);
+                if (i != 0) pre->next = cur;
+                pre = cur;
+            }
+            cur->next = nullptr;
+        }
+    }
+};
