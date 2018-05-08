f69f067947e0ee71af98ee24542e6570c7db8e88
winhows5 <wangwh258@gmail.com>
2018-05-09 00:27:46 +0800

    Update ans1.cpp

--- 0128. Longest Consecutive Sequence/ans1.cpp
+++ 0128. Longest Consecutive Sequence/ans1.cpp
@@ -1,6 +1,7 @@
 /* time cost : 7ms (99.79%) */
 /* time complexity: O(n)    */
 
+// I found a missed test case! 🤩
 static int x = [](){
     std::ios::sync_with_stdio(false);
     cin.tie(NULL);
@@ -12,48 +13,31 @@ public:
     int longestConsecutive(vector<int>& nums) {
         unordered_map<int, int> cons;
         int ans = 0;
+        int tmp_l, tmp_r;
         for (int& c : nums) {
-            if (cons.find(c) != cons.end())
+            cout << c;
+            if (cons[c] != 0)
                 continue;
             else {
                 cons[c] = 1;
-                if (c == INT_MAX) 
-                    if (cons.find(c-1) != cons.end()) {
-                        int tmp = cons[c-1];
-                        cons[c] = tmp+1;
-                        cons[c-tmp] = tmp+1;
-                        ans = max(ans, tmp+1);
+                ans = max(ans, 1);
+                if (c == INT_MAX) {
+                    tmp_l = cons[c-1];
+                    tmp_r = 0;
                 }
-                else if (c == INT_MIN) 
-                    if (cons.find(c+1) != cons.end()) {
-                        int tmp = cons[c+1];
-                        cons[c] = tmp+1;
-                        cons[c+tmp] = tmp+1;
-                        ans = max(ans, tmp+1);
+                else if (c == INT_MIN) {
+                    tmp_l = 0;
+                    tmp_r = cons[c+1];
                 }
                 else {
-                    if (cons.find(c-1) != cons.end() && cons.find(c+1) != cons.end()) {
-                        int tmp_l = cons[c-1];
-                        int tmp_r = cons[c+1];
-                        cons[c-tmp_l] = tmp_l + tmp_r + 1;
-                        cons[c+tmp_r] = tmp_l + tmp_r + 1;
-                        ans = max(ans, tmp_l + tmp_r + 1);
-                    }
-                    else if (cons.find(c-1) != cons.end()) {
-                        int tmp = cons[c-1];
-                        cons[c] = tmp+1;
-                        cons[c-tmp] = tmp+1;
-                        ans = max(ans, tmp+1);
-                    }
-                    else if (cons.find(c+1) != cons.end()) {
-                        int tmp = cons[c+1];
-                        cons[c] = tmp+1;
-                        cons[c+tmp] = tmp+1;
-                        ans = max(ans, tmp+1);
-                    }
+                    tmp_l = cons[c-1];
+                    tmp_r = cons[c+1];   
                 }
+                cons[c-tmp_l] = tmp_l + tmp_r + 1;
+                cons[c+tmp_r] = tmp_l + tmp_r + 1;
+                ans = max(ans, tmp_l + tmp_r + 1);
             }
-        return ans;
         }
+        return ans;
     }
 };
