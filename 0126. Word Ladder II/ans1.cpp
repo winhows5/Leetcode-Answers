c98afeda6eea615b87aeb06b78f5433701e40af5
winhows5 <wangwh258@gmail.com>
2018-05-17 19:40:47 +0800

    Update ans1.cpp

--- 0126. Word Ladder II/ans1.cpp
+++ 0126. Word Ladder II/ans1.cpp
@@ -1,5 +1,5 @@
-/* time cost: ms (%) */
-/* time complexity: O() */
+/* time cost: 37ms (97.10%) */
+/* time complexity: O(nlog(n)) */
 
 static int x = [](){
     std::ios::sync_with_stdio(false);
@@ -9,8 +9,8 @@ static int x = [](){
 
 class Solution {
 public:
-    void resList(string start, string end, unordered_map<string, vector<string>> map_words, 
-             vector<string> valid_list, vector<vector<string>> res) {
+    void resList(string start, string end, unordered_map<string, vector<string>>& map_words, 
+             vector<string>& valid_list, vector<vector<string>>& res) {
         if (start == end) {
             res.push_back(valid_list);
             return;
@@ -23,29 +23,33 @@ public:
         }
     }
     vector<vector<string>> findLadders(string beginWord, string endWord, vector<string>& wordList) {
-        vector<vector<string>> ans;
+        vector<vector<string>> res;
         unordered_set<string> wordSet(wordList.begin(), wordList.end());  
-        if (wordSet.find(endWord) == wordSet.end()) return ans;
+        if (wordSet.find(endWord) == wordSet.end()) return res;
         wordSet.erase(endWord);
-        wordSet.erase(startWord);
+        wordSet.erase(beginWord);
         unordered_set<string> beginSet, endSet, *pbeginSet, *pendSet;
         beginSet.insert(beginWord);
         endSet.insert(endWord);
         
         unordered_map<string, vector<string>> map_words;
-        vector<string> valid_list;
+        bool done = false;
+        bool flip = false;
         
         // BFS
         while(!beginSet.empty() && !endSet.empty()) {
-
+            
             if (beginSet.size() > endSet.size()) {
                 pbeginSet = &endSet;
                 pendSet = &beginSet;
+                flip = true;
             }
             else {
                 pbeginSet = &beginSet;
                 pendSet = &endSet;
+                flip = false;
             }
+            
             unordered_set<string> tmpSet;
             for (unordered_set<string>::iterator it = pbeginSet->begin(); 
                 it != pbeginSet->end(); ++it) {
@@ -54,70 +58,31 @@ public:
                     char tmp_c = tmp[j];
                     for (int k = 0; k < 26; ++k) {
                         tmp[j] = 'a' + k;
-                        if (pendSet->find(tmp) != pendSet->end()) return cnt;
-                        if (tmp[j] != tmp_c && wordSet.find(tmp) != wordSet.end()) {
+                        string key_word = flip ? tmp : *it;
+                        string val_word = flip ? *it : tmp;
+                        if (pendSet->find(tmp) != pendSet->end()) {
+                            map_words[key_word].push_back(val_word);
+                            done = true;
+                        }
+                        if (!done && tmp[j] != tmp_c && wordSet.find(tmp) != wordSet.end()
+                           && pbeginSet->find(tmp) == pbeginSet->end()) {
                             tmpSet.insert(tmp);
-                            wordSet.erase(tmp);
+                            map_words[key_word].push_back(val_word);
                         }
                     }
                     tmp[j] = tmp_c;
                 }
             }
+            if (done) break;
             swap(*pbeginSet, tmpSet);
+            for (auto it = tmpSet.begin(); it != tmpSet.end(); ++it) {
+                wordSet.erase(*it);
+            }
         }
-        
+
         // DFS
-        resList(start, end, map_words, valid_list, res);
-        return ans;
+        vector<string> valid_list{beginWord};
+        resList(beginWord, endWord, map_words, valid_list, res);
+        return res;
     }
 };
-
-    
-    // remove words on current both ends from the dict
-    dict.removeAll(set1);
-    dict.removeAll(set2);
-    
-    // as we only need the shortest paths
-    // we use a boolean value help early termination
-    boolean done = false;
-    
-    // set for the next level
-    Set<String> set = new HashSet<String>();
-    
-    // for each string in end 1
-    for (String str : set1) {
-      for (int i = 0; i < str.length(); i++) {
-        char[] chars = str.toCharArray();
-        
-        // change one character for every position
-        for (char ch = 'a'; ch <= 'z'; ch++) {
-          chars[i] = ch;
-          
-          String word = new String(chars);
-          
-          // make sure we construct the tree in the correct direction
-          String key = flip ? word : str;
-          String val = flip ? str : word;
-              
-          List<String> list = map.containsKey(key) ? map.get(key) : new ArrayList<String>();
-              
-          if (set2.contains(word)) {
-            done = true;
-            
-            list.add(val);
-            map.put(key, list);
-          } 
-          
-          if (!done && dict.contains(word)) {
-            set.add(word);
-            
-            list.add(val);
-            map.put(key, list);
-          }
-        }
-      }
-    }
-    
-    // early terminate if done is true
-    return done || helper(dict, set2, set, map, !flip);
-  }
