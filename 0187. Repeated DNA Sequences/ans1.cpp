483454ac4d3449963e98a00b38d5ab68ca8784bf
winhows5 <wangwh258@gmail.com>
2019-10-25 01:54:58 +0800

    0188

---
+++ 0187. Repeated DNA Sequences/ans1.cpp
@@ -0,0 +1,25 @@
+/* time cost: 8ms (74.23%) */
+/* time complexity: O(n) */
+
+class Solution {
+public:
+
+    // Too Slow (164ms)
+    vector<string> findRepeatedDnaSequences(string s) {
+        vector<string> res;
+        if (s.length() < 10) return res; 
+        map<string, int> find;
+        for (int i=0; i<s.length()-9; ++i) {
+            string sub = s.substr(i,10);
+            if (find.find(sub) != find.end()) {
+                find[sub]++;
+            } else {
+                find[sub] = 0;
+            }
+        }
+        for (map<string, int>::value_type e : find) {
+            if (e.second > 0) res.push_back(e.first);
+        }
+        return res;
+    }
+};
